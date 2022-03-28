ad00a33a2bbe6178abedac92fd2e9ed47fb3190b
henrypadua <henrybpj@live.com>
2022-03-28 00:57:09 -0300

    Adjusted for deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL: 'http://localhost:3000'
+   baseURL:
+      process.env.NODE_ENV === 'production'
+         ? 'https://henrypadua.github.io/invoicexpress-challenge/'
+         : 'http://localhost:3000'
 })
 
 export default api
